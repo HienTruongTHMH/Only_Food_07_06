ca92afbe6295acd7e8418238d53ffc54e6be28be
HienTruong <hien0934752432@gmail.com>
2025-06-08 01:16:10 +0700

    tạm thời để build

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -83,7 +83,7 @@ const LoginForm = () => {
             className="ml-2 w-full outline-none bg-transparent"
             disabled={loading}
           />
-          <button
+          {/* <button
             type="button"
             onClick={() => setShowPassword(!showPassword)}
             className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500 hover:text-gray-700"
@@ -94,7 +94,7 @@ const LoginForm = () => {
             ) : (
               <Eye className="w-5 h-5" />
             )}
-          </button>
+          </button> */}
         </div>
         <button
           type="submit"
